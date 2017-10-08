c1ba266d750df5b0d006dd1430492b164c025a31
Jace Roldan <jtroldan@up.edu.ph>
2017-10-09 00:05:29 +0800

    Clean up Square and PacMan classes
    Added other functionality to the PacMan classes for vector use; No executable yet.

--- group_mp.cpp
+++ group_mp.cpp
@@ -2,8 +2,13 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <cmath>
+#include <algorithm>
 
 using namespace std;
+
+int computeManhattanDistance(int, int, int, int);
+int computeStraightLineDistance(int, int, int, int, int);	
 
 enum {
 	WALL = '%',
@@ -13,61 +18,76 @@ enum {
 	MD = 1,
 	SLD = 2
 };
-class Pos {
-private:
-	int row, col;
-}
-
-
+	
 class Square {
 private:
 	char content;
-	int row, col;
+
+	// position variables
+	int row;
+	int col;
+
 	bool visited;
 	int cumulativeCost;
-	int heuristic;	
+	int heuristic;
 	Square *parent;
 public:
 	Square() {
-		row = col = 0;
+		row = 0;
+		col = 0;
 		visited = false;
-		item = 0;
+		content = 0;
 	}
+
 	Square(int row, int col,char item) {
-		this->item = item;
-		this->row = row;
-		this->col = col;
+		row = row;
+		col = col;
 		visited = false;
+		this->content = item;		
 	}
-	bool isPassable() { return (item == WALL) ? false : true; }
+
+	bool isPassable() { return (content == WALL) ? false : true; }
 	bool isVisited() { return visited; }
-	char getItem() { return item; }
+
+	char getItem() { return content; }
 	int getRow() { return row; }
 	int getCol() { return col; }
-	void setVisited(){ visited = (!visited) ? true : false; }
+	int getHeuristic() { return heuristic; }
+	int getCumulative() { return cumulativeCost; }
+
+	void setVisited(){ visited = (!visited) ? true : false; }	
 	void setSquare(int,int,int,int,char);
-	int getHeuristic();
-	int getCumulative();
+	
+	void setHeuristic();
+
+	void setCumulative() { cumulativeCost = parent->cumulativeCost + 1; }
 	Square operator=(Square);
 };
 
-int getHeuristic(int type) {
+void Square::setHeuristic(int type, int destX, int destY) {
 	if(type == MD) {
-
+		heuristic = computeManhattanDistance(this->x, this->y, destX, destY);
 	} 
+	else if(type == SLD) {
+		heuristic = computeStraightLineDistance(this->x, this->y, destX, destY, 1);
+	}
 	else {
-
+		cout << "Heuristic type not valid. Resend arguments." << endl;
 	}
 }
 
-int getCumulative() {
-
+Square Square::operator=(Square sq){
+	this->content = sq.content;
+	this->row = sq.row;
+	this->col = sq.col;
+	this->visited = sq.visited;
+	this->cumulativeCost = sq.cumulativeCost;
+	this->heuristic = sq.heuristic;
 }
 
-// heuristic,cumulative,total costs
-void Square::setSquare() {
 
-}
+// TODO; FINISH
+
 
 class Maze {
 private:
@@ -84,7 +104,7 @@ public:
 
 		for(int i = 0, k = 0; i < length; i++) {
 			for(int j = 0; j < width; j++, k++) {				
-				this->maze[i][j].setSquare(i, j, mazeInFile[k]);				
+				this->maze[i][j].setSquare(i, j, mazeInFile[k]);
 			}
 		}
 		cout<<toString()<<endl;		
@@ -95,22 +115,108 @@ public:
 
 		delete[] maze;
 	}
-	Square getSquare(int,int);
-	void setVisited(int,int);
-	void setPath(int,int);
+
+	Square getSquare(int x ,int y) { return maze[x][y]; }
+	void setVisited(int x, int y) { maze[x][y].setVisited(); }
+	// void setPath(int,int);
 	int getLength() { return length; }
 	int getWidth() { return width; }
-	string toString();
+
+	string toString() {
+		for(int i = 0; i < length; i++){
+			for(int j = 0; j < width; j++){
+				cout<<maze[i][j];
+			}
+			cout << endl;
+		}
+	}
+
 };
 
+
 class PacMan {
+private:
+	Maze m;
 	vector<Square> parentList;
 	vector<Square> closedList;
 	vector<Square> openList;
-
-
+	Square current;
+	Square goal;
+public:
+	bool addSquare(int,int);
+	void scoutDirections();
+	Square getLowestCost();
+	void switchClosed();
+	bool fin();
 };
+
+bool PacMan::addSquare(int x, int y) {
+	if(x > m.getLength() || y m.getWidth() || x < 0 || y < 0) {
+		return false;
+	} else if (m.getSquare(x, y).getItem() == WALL) {
+		return false;
+	} else {
+		openList.add(m.getSquare(x,y));
+	}
+}
+
+Square PacMan::getLowestCostSquare() {
+	int min = numeric_limits<int>::infinity();
+	Square current;
+
+	for(vector<Square>::iterator it = openList().begin; it != openList.end(); it++) {
+		if(it->getCumulative() < min) {
+			min = it->getCumulative();
+			current = *(it);
+		}
+	}
+
+	return current;
+}
+
+bool PacMan::switchClosed() {
+	closedList.add(getLowestCostSquare());
+	current = *(closedList.begin());	
+}
+
+void PacMan::scoutDirections() {
+	addSquare(current.getRow()-1,current.getCol());
+	addSquare(current.getRow(),current.getCol()-1);
+	addSquare(current.getRow()+1,current.getCol());
+	addSquare(current.getRow(),current.getCol()+1);
+}
+
+bool PacMan::fin() {
+	if(current.getRow() == goal.getRow() && current.getCol() == goal.getCol()){
+		return true;
+	}
+	return false;
+}
 
 int main() {
 	return 0;
+}
+
+/*
+	pacman starts at Pos current
+	pacman checks surroundings
+		if passable, it adds to the closed list
+		<Square>.setParent(current);
+		<Square>.setCumulativeCosts;
+		closedList.add(<Square>)
+		checkIfBetterPathFunction();
+
+
+		
+*/ 
+
+
+// produce heuristics
+
+int computeManhattanDistance(int srcX, int srcY, int destX, int destY) {
+	return abs(destX-srcX) + abs(destY-srcY);
+}
+
+int computeStraightLineDistance(int srcX, int srcY, int destX, int destY, int cost) {
+	return max(abs(destX-srcX),abs(destY-srcY));
 }
